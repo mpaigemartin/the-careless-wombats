68977e2a4093072f1bc1eef85930d280f0422295
Barry Scott Rollan <b.scott.rollan@gmail.com>
2019-03-24 17:29:42 -0400

    main page elements mocked up

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import NavBar from './component/NavBar'
+import Header from './component/Header'
 import BusinessDirectory from './component/BusinessDirectory';
+import SearchBar from './component/SearchBar';
+import FilterButtonRow from './component/FilterButtonRow';
+// import MapBox from './component/MapBox';
+import GoogleMap from './component/GoogleMap';
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-     <div>
-       <NavBar />
-       <BusinessDirectory/>
-
+     <div className="container">
+        <Header/>
+        <SearchBar/>
+        <FilterButtonRow/>
+        <GoogleMap/>
+        {/* <MapBox/> */}
+        <BusinessDirectory/>
      </div>
     );
   }
--- client/src/component/BusinessDirectory.js
+++ client/src/component/BusinessDirectory.js
@@ -4,6 +4,7 @@ import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
 import "../component/Styles.css";
 
 const styles = theme => ({
@@ -15,7 +16,7 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 5,
     width: "100%",
     height: "100%",
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.secondary
   },
   control: {
     padding: theme.spacing.unit * 2
@@ -27,8 +28,8 @@ class BusinessDirectory extends React.Component {
     const { classes } = this.props;
 
     return (
-      <Grid container className={classes.root}>
-        <Grid item xs={4}>
+      <Grid container className={classes.root} id="businessList">
+        <Grid>
           <Grid
             container
             spacing={16}
@@ -36,24 +37,36 @@ class BusinessDirectory extends React.Component {
             direction="column"
             justify="center"
           >
-            <Grid item>
-              <Paper
-                className={classes.root}
-                elevation={1}
-                style={{ paddingTop: 2, paddingBottom: 2 }}
-              >
-              <Typography variant="h5" component="h3">
-                The Fuzzy Mule
-              </Typography>
-              <Typography variant="h6" component="h3">
+            <Grid item className="businessPaper clearfix">
+              <Paper className={classes.root} elevation={1}>
+                <Typography variant="h5" component="h3">
+                  The Fuzzy Mule
+                </Typography>
+                <Typography variant="h6" component="h3">
                   Tuesday NasCar Trivia Night
                 </Typography>
-               <Typography component="p">
-                  Try our Possum Burger with a flight of moonshine
+                <Typography component="p">
+                  Try our Possum Burger with a flight of moonshine. Lorem ipsum
+                  dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id
+                  est laborum<Button
+                  variant="contained"
+                  color="secondary"
+                  className={classes.button}
+                  style={{position: "relative", float: "right"}}
+                >
+                  check us out!
+                </Button>{" "}
                 </Typography>
+                
               </Paper>
-              </Grid>
-            <Grid item>
+            </Grid>
+            <Grid item className="businessPaper">
               <Paper className={classes.root} elevation={1}>
                 <Typography variant="h5" component="h3">
                   Mabel's Bar & Chainsaw Repair
@@ -63,8 +76,8 @@ class BusinessDirectory extends React.Component {
                 </Typography>
               </Paper>{" "}
             </Grid>
-          
-            <Grid item>
+
+            <Grid item className="businessPaper">
               <Paper className={classes.root} elevation={1}>
                 <Typography variant="h5" component="h3">
                   The Duke and Duck
@@ -72,9 +85,7 @@ class BusinessDirectory extends React.Component {
                 <Typography variant="h6" component="h3">
                   Get pissed the English way
                 </Typography>
-                <Typography component="p">
-                  Bip Bip Cheerio
-                </Typography>
+                <Typography component="p">Bip Bip Cheerio</Typography>
               </Paper>{" "}
             </Grid>
           </Grid>
