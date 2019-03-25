8d7bef9318a68f29a43a5ad5a7c7a2e6d3a29b6d
Nihit Tiwari <nihitter05@gmail.com>
2019-03-25 13:07:48 -0400

    possible changes

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost/', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/mydb', { useNewUrlParser: true });
 
-require('./routes/api-routes')(app);
-require('./routes/html-routes')(app);
+require('./routes/apiRoutes')(app);
 
 
 app.listen(PORT, function() {
