46252af1eadec97fa08fdc4a94474ce318a3685d
Cody Goss <c.ward.goss@gmail.com>
2019-03-25 15:02:35 -0400

    modal working

--- client/src/component/BusinessDirectory.js
+++ client/src/component/BusinessDirectory.js
@@ -78,10 +78,10 @@ class BusinessDirectory extends React.Component {
                   Try our Possum Burger with a flight of moonshine. Lorem ipsum
                   dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi
-                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                  nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                  minim veniam, quis nostrud ullamco laboris nisi ut aliquip ex
+                  ea commodo consequat. Duis aute irure dolor in reprehenderit
+                  in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident
                   {/* <Button
                     variant="contained"
                     color="secondary"
