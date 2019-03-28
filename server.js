e8516603bf18d04a9b4e41ecf9a4347d045fd9f1
Paige <mpaigemartin@gmail.com>
2019-03-28 11:58:06 -0400

    fixes the package

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,13 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost/nightOwl', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/nightOwl", { useNewUrlParser: true });
 
-
-require('./routes/apiRoutes')(app);
-
-
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log(`You are now listening to your favorite pirate radio station, Port ${PORT}! Sail away, mateys!`)
-})
+  console.log(
+    `You are now listening to your favorite pirate radio station, Port ${PORT}! Sail away, mateys!`
+  );
+});
